111c439bdd8234cc3bb003f84d8a55c69ba20c56
hirsch <adwayweb@gmail.com>
2021-05-13 23:38:03 +0430

    first commit on the proj before set on github

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,67 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+      <span>Created by Tesla</span>
+      <v-spacer></v-spacer>
+
+      <v-btn href="#" target="_blank" text>
+        <span class="mr-2 hidden-sm-and-down">Technosun</span>
+        <!-- <v-icon>mdi-open-in-new</v-icon> -->
+      </v-btn>
+
+      <v-img
+        alt="Vuetify Name"
+        class="shrink mt-1"
+        contain
+        min-width="100"
+        src="./assets/technosun-logo-rebranding-50.png"
+        width="120"
+      />
+    </v-app-bar>
+
+    <v-main>
+      <HelloWorld style="display: none" />
+      <Home />
+      <Profile />
+      <v-autocomplete v-model="values" :items="items"> </v-autocomplete>
+    </v-main>
+
+    <v-footer app>
+      <!-- -->
+      <p>this is footer for testing</p>
+      <hr />
+      <p style="display: block">Cteated by Hirsch adway</p>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HelloWorld from "./components/HelloWorld";
+import Home from "./components/Home";
+import Profile from "./components/Profile";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    HelloWorld,
+    Home,
+    Profile,
+  },
+
+  data: () => ({
+    items: ["life", "good", "boo", "foo", "hirsch", "javad"],
+  }),
+};
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col
+        v-for="(garant, i) in garants"
+        :key="i"
+        justify="center"
+        style="border-style: dashed"
+        class="text-end"
+      >
+        <span>
+          {{ garant.name }}
+        </span>
+        <a href="#" class="subheading mx-3" target="_blank">
+          {{ garant.search }}
+        </a>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data: () => ({
+    garants: [
+      {
+        name: "good",
+        search: "جستجو بر اساس کالا",
+      },
+      {
+        name: "person",
+        search: "جستجو بر اساس فرد",
+      },
+    ],
+  }),
+};
+</script>
+
+<style>
+</style>
